c85cd0c18d3aa6cad095fb94985fed09a3e20dfe
MrMavni <144116832+MrMavni@users.noreply.github.com>
2024-01-16 19:57:52 +0200

    Update deploy.sh
    now includes prepare.sh

--- deploy.sh
+++ deploy.sh
@@ -55,8 +55,27 @@ update_password "POSTGRES_PASSWORD"
 update_guacadmin_password
 
 # Run prepare.sh script
-echo "Running prepare.sh..."
-sudo ./prepare.sh
+echo "preparing Database and Folders"
+#!/bin/sh
+#
+# check if docker is running
+if ! (docker ps >/dev/null 2>&1)
+then
+	echo "docker daemon not running, will exit here!"
+	exit
+fi
+echo "Preparing folder init and creating ./init/initdb.sql"
+mkdir ./init >/dev/null 2>&1
+mkdir -p ./nginx/ssl >/dev/null 2>&1
+chmod -R +x ./init
+docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > ./init/initdb.sql
+sudo chown -R 1000:1001 ./record/
+sudo chmod -R 2750 ./record/
+echo "done"
+echo "Creating SSL certificates"
+openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=DE/ST=BY/L=Hintertupfing/O=Dorfwirt/OU=Theke/CN=www.createyourown.domain/emailAddress=docker@createyourown.domain'
+echo "You can use your own certificates by placing the private key in nginx/ssl/self-ssl.key and the cert in nginx/ssl/self.cert"
+echo "done"
 
 # Stop the current Docker Compose deployment
 echo "Stopping current Docker Compose deployment..."
@@ -66,15 +85,8 @@ docker-compose -f "$DOCKER_COMPOSE_FILE" down
 echo "Deploying updated Docker Compose configuration..."
 docker-compose -f "$DOCKER_COMPOSE_FILE" up -d
 
-# Change permissions to allow container to write to recording folder
-echo "Setting recording folder permissions"
-sudo chown -R 1000:1001 ./record/
-sudo chmod -R 2750 ./record/
-
 # Print all generated credentials
 echo "Generated Credentials:"
 for key in "${!credentials[@]}"; do
     echo "$key: ${credentials[$key]}"
 done
-
-# Cleanup credentials after
